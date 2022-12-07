b721f8717e5657e4d077c079e4591289508dea0f
DimVinich <DmitrijVS@gmail.com>
2022-12-07 23:05:03 +0300

    Перенос на другой комп

--- LifeSpot/Static/JS/about.js
+++ LifeSpot/Static/JS/about.js
@@ -1,7 +1,7 @@
 ﻿/*
 * Запросим пользовательский ввод
 * и сохраним отзыв в объект
-* 
+* т.к поменяли функцию выдачи результата,то ниже приведённое рабоать не будет
 * */
 function getReview() {
     // Создадим объект
@@ -27,12 +27,63 @@ function getReview() {
 }
 
 /*
-* Запишем отзыв на страницу 
-* 
+* Оставить комментарий
+*
+* */
+function getComment() {
+    // Создаем объект обычного комментария
+    let comment = {}
+
+    // Запросим имя
+    comment.author = prompt("Как вас зовут ?")
+    if (comment.author == null) {
+        return
+    }
+
+    // Запросим текст
+    comment.text = prompt("Оставьте отзыв")
+    if (comment.text == null) {
+        return
+    }
+
+    // Сохраним текущее время
+    comment.date = new Date().toLocaleString()
+
+    // Запросим,, хочет ли пользователь оставить полноценный отзыв или это будет обычный комментарий
+    let enableLikes = confirm('Разрешить пользователям оценивать ваш отзыв?')
+
+    if (enableLikes) {
+        // Создадим для отзыва новый объект из прототипа - комментария
+        let review = Object.create(comment)
+        // и добавим ему нужное свойство
+        review.rate = 0;
+
+        // Добавляем отзыв с возможностью пользовательских оценок
+        writeReview(review)
+    } else {
+        // Добавим простой комментарий без возможности оценки
+        writeReview(comment)
+    }
+}
+
+/*
+ 
+/*
+* Запишем объект на страницу
+*
 * */
 const writeReview = review => {
+    let likeCounter = '';
+
+    // Для проверки, является ли объект отзывом, используем свойство hasOwnProperty
+    if (review.hasOwnProperty('rate')) {
+        likeCounter += '           <b style="color: chocolate">Рейтинг:</b>   ' + review.rate;
+    }
+
+    // Запишем результат
     document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
-        `<p> <i> <b>${review['userName']}</b>  ${review['date']}</i></p>` +
-        `<p>${review['comment']}</p>`  +
+        `<p> <i> <b>${review['author']}</b>  ${review['date']}${likeCounter}</i></p>` +
+        `<p>${review['text']}</p>` +
         '</div>';
 }
+
